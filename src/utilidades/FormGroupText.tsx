aa409f47a2d4bb985190b2af1d21a60b63f1e58f
Enzo Zarate <enzozarateok66@gmail.com>
2022-12-11 18:52:39 -0300

    type

--- src/utilidades/FormGroupText.tsx
+++ src/utilidades/FormGroupText.tsx
@@ -1,26 +1,31 @@
 import { Field, ErrorMessage, useFormikContext } from "formik";
 import MostrarErrorCampo from "./MostrarErrorCampo";
 import React from "react";
+import { string } from "yup";
 
 
 export default function FormGroupText(props: formGroupTextProps) {
 
     const {values} = useFormikContext<any>();
     return (
-        <>
-            <div className='form-group'>
-               {props.label ?  <label htmlFor={props.campo}>{props.label}</label> : null}
-                <Field name={props.campo} className='form-control' placeholder={props.placeholder} value={values[props.campo] ?? ""} />
-                <ErrorMessage name={props.campo}>{mensaje =>
-                    <MostrarErrorCampo mensaje={mensaje} />}
-                </ErrorMessage>
-            </div>
-            
-        </>
-    )
+        <div className="form-group">
+        {props.label ? <label htmlFor={props.campo}>{props.label}</label> : null} 
+        <Field type={props.type} name={props.campo} className="form-control" 
+        placeholder={props.placeholder} />
+        <ErrorMessage name={props.campo}>{mensaje =>
+            <MostrarErrorCampo mensaje={mensaje} />
+        }</ErrorMessage>
+    </div>
+)
 }
+
 interface formGroupTextProps{
-    campo: string,
-    label?: string,
-    placeholder?: string
+campo: string;
+label?: string;
+placeholder?: string;
+type: 'text' | 'password';
+}
+
+FormGroupText.defaultProps = {
+type: 'text'
 }
