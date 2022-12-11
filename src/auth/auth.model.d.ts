4d5f7f459f3f48a5e866ee55c03aa2ea4084819a
Enzo Zarate <enzozarateok66@gmail.com>
2022-12-11 18:53:33 -0300

    credenciales

--- src/auth/auth.model.d.ts
+++ src/auth/auth.model.d.ts
@@ -2,3 +2,12 @@ export interface claim{
     nombre: string;
     valor: string;
 }
+export interface credencialesUsuario{
+    email: string;
+    password: string;
+}
+
+export interface respuestaAutenticacion{
+    token: string;
+    expiracion: Date;
+}
